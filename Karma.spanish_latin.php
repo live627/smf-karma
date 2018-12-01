976f40bba5886d2699e8c47732851740fb00e892
Bomber Code <contacto@bombercode.net>
2018-12-01 18:25:50 -0300

    Translation to Latin Spanish!

---
+++ Karma.spanish_latin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Karma.english
+ *
+ * The purpose of this file is to fill the language entries with English.
+ * @package   Karma mod
+ * @version   1.0 Alpha
+ * @author    John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ * Spanish translation https://www.bombercode.net Copyright 2014-2018
+ */
+// General Strings
+$txt['karma'] = 'Karma';
+$txt['karmaLabel'] = 'Karma:';
+$txt['karmaSmiteLabel'] = '[castigar]';
+$txt['karmaApplaudLabel'] = '[aplaudir]';
+// Errors
+$txt['not_enough_posts_karma'] = 'Lo sentimos, no tienes suficientes publicaciones para modificar el karma, necesitas al menos %1$d.';
+$txt['cant_change_own_karma'] = 'Lo sentimos, no se te permite modificar tu propio karma.';
+$txt['karma_wait_time'] = 'Lo sentimos, no puedes repetir una acción de karma sin esperar. %1$s %2$s.';
+$txt['cannot_karma_edit'] = 'No est&aacute; permitido modificar el karma de otras personas.';
+?>
