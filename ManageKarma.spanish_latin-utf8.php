4d7388ff384a3bec978afa705e2432f3fbb928b7
Bomber Code <contacto@bombercode.net>
2018-12-01 18:17:13 -0300

    Translation to Latin Spanish!

---
+++ ManageKarma.spanish_latin-utf8.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Karma.spanish_latin
+ *
+ * The purpose of this file is to fill the language entries with English.
+ * @package   Karma mod
+ * @version   1.0 Alpha
+ * @author    John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license   http://opensource.org/licenses/MIT MIT
+ *
+ * Spanish translation https://www.bombercode.net Copyright 2014-2018
+ */
+// Admin Panel Strings
+$txt['mods_cat_karma'] = 'Karma';
+$txt['karmaMode'] = 'Modo karma';
+$txt['karma_options'] = 'Desactivar karma | Activar total de Karma | Activar karma positivo / negativo';
+$txt['karmaMinPosts'] = 'Establecer los mensajes mínimos necesarios para modificar el karma.';
+$txt['karmaWaitTime'] = 'Establecer el tiempo de espera en horas';
+$txt['karmaTimeRestrictAdmins'] = 'Restringir a los administradores el tiempo de espera';
+// Permissions
+$txt['permissionname_karma_edit'] = 'Cambiar el karma de otras personas';
+$txt['permissionhelp_karma_edit'] = 'El karma es una característica que muestra la popularidad de un miembro. Para poder utilizar esta función, debe estar activado en "Características y opciones". Este permiso permitirá a un miembro de un grupo emitir un voto. Este permiso no tiene efecto en los invitados.';
+// Reports
+$txt['group_perms_name_karma_edit'] = 'Cambiar el karma de otras personas.';
+?>
