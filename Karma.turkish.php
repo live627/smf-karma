56357ce1f076b5543db83b5c50e57127eea5cb4d
John Rayes <live627@gmail.com>
2018-12-11 13:33:06 -0700

    Create Karma.turkish.php

---
+++ Karma.turkish.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Karma.turkish translation by snrj (http://smf.konusal.com)
+ *
+ * The purpose of this file is to fill the language entries with Turkish.
+ * @package   Karma mod
+ * @version   1.0 Alpha
+ * @author    John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+// General Strings
+$txt['karma'] = 'Karma';
+$txt['karmaLabel'] = 'Karma:';
+$txt['karmaSmiteLabel'] = '<span class="generic_icons unlike" title="Azalt"></span>';
+$txt['karmaApplaudLabel'] = '<span class="generic_icons like" title="Arttır"></span>';
+
+// Errors
+$txt['not_enough_posts_karma'] = 'Üzgünüz, karma değiştirmek için yeterli mesajınız yok - en az %1$d mesaja ihtiyacınız var.';
+$txt['cant_change_own_karma'] = 'Üzgünüz, kendi karmanızı değiştirmeye izin verilmez..';
+$txt['karma_wait_time'] = 'Üzgünüm, bir karma eylemini  %1$s %2$s beklemeden tekrar edemezsin.';
+$txt['cannot_karma_edit'] = 'Başkalarının karmalarını değiştirmeye izin verilmez.';
