291faecfbbf8f49de1d31ad256fa6504f4ff474f
John Rayes <live627@gmail.com>
2018-12-11 13:42:41 -0700

    Create ManageKarma.turkish.php

---
+++ ManageKarma.turkish.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Karma.turkish translation by snrj (http://smf.konusal.com)
+ *
+ * The purpose of this file is to fill the language entries with Turkish.
+ * @package   Karma mod
+ * @version   1.0 Alpha
+ * @author    John Rayes <live627@gmail.com>
+ * @copyright Copyright (c) 2014, John Rayes
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+// Admin Panel Strings
+$txt['mods_cat_karma'] = 'Karma';
+
+$txt['karmaMode'] = 'Karma modu';
+$txt['karma_options'] = 'Karmayı devre dışı bırak|Toplam Karmayı etkinleştir|Karma pozitif/negatif\'i etkinleştir';
+$txt['karmaMinPosts'] = 'Karmayı değiştirmek için gereken minimum mesaj sayısı';
+$txt['karmaWaitTime'] = 'Saat cinsinden bekleme süresini ayarla';
+$txt['karmaTimeRestrictAdmins'] = 'Yöneticilerinde bekleme süresini kısıtlayın';
+
+// Permissions
+$txt['permissionname_karma_edit'] = 'Başkalarının karmalarını değiştir';
+$txt['permissionhelp_karma_edit'] = 'Karma, bir üyenin popülaritesini gösteren bir özelliktir. Bu özelliği kullanabilmek için, \ 'Özellikler ve Seçenekler \' seçeneğinde etkinleştirmeniz gerekir. Bu izin, bir üye grubunun oy kullanmasına izin verecektir. Bu iznin ziyaretçilere etkisi yoktur.';
+
+// Reports
+$txt['group_perms_name_karma_edit'] = 'Başkalarının karmalarını değiştir';
